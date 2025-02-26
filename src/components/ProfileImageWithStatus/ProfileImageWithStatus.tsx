b850faf60490b8441a317ec63a3547bc209259db
Almaze Lema <chupzilemma12@gmail.com>
2025-02-26 14:27:09 -0500

    we've now imported our child component and received image props from it. we handle this exchange, and then set the profile image using a slice of state, profileImageURL.

--- src/components/ProfileImageWithStatus/ProfileImageWithStatus.tsx
+++ src/components/ProfileImageWithStatus/ProfileImageWithStatus.tsx
@@ -1,8 +1,8 @@
 import { AppointmentStatus } from "../../models/appointments";
 import { StatusBadge } from "../StatusBadge/StatusBadge";
 import './ProfileImageWithStatus.css';
-// import defaultAvatarURL from '../../../public/default-avatar.jpg';
-
+import { ImageUploadDialog } from "./ImageUploadDialog/ImageUploadDialog";
+import { useState } from "react";
 
 export interface ProfileImageWithStatusProps {
     status: AppointmentStatus;
@@ -12,11 +12,20 @@ export interface ProfileImageWithStatusProps {
 
 const defaultAvatarURL = '/default-avatar.jpg';
 
-
 export const ProfileImageWithStatus: React.FC<ProfileImageWithStatusProps> = ({ status, subText, imageURL }) => {
+    // Create state for the profile's image
+    const [profileImageURL, setProfileImageURL] = useState<string>(() => imageURL || defaultAvatarURL);
+
+    const handleImageUpload = (file: File) => {
+        console.log("Uploaded file received in parent:", file);
+        // You can send this to an API or update state
+        const imageUrl = URL.createObjectURL(file);
+        setProfileImageURL(imageUrl);
+    };
+
     return (
         <span className="ProfileImageWithStatus">
-            <img className="profile-image" src={imageURL || defaultAvatarURL} alt={subText} />
+            <ImageUploadDialog imageURL = {profileImageURL} subText= {subText} onImageUpload={handleImageUpload}/>
             <span className="subText">{subText}</span>
             <StatusBadge status={status} />
         </span>
